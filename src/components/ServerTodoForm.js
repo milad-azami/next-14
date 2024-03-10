ca01747cca4dc8baf9680e09ca2d502dc45763d0
Milad Azami <miladazami120@gmail.com>
2024-03-10 22:38:23 +0330

    revalidatePath in server component

--- src/components/ServerTodoForm.js
+++ src/components/ServerTodoForm.js
@@ -1,34 +1,37 @@
+import { revalidatePath } from "next/cache";
+
 import Todo from "@/models/Todo";
 import connectDB from "@/utils/connectDB";
 
 function ServerTodoForm() {
   const addTodo = async (formData) => {
     "use server";
+
     await connectDB();
 
     const title = formData.get("title");
     const description = formData.get("description");
 
     const todo = await Todo.create({ title, description });
+    revalidatePath("/");
+
     console.log(todo);
   };
 
   return (
-    <>
-      <div>
-        <form action={addTodo}>
-          <div>
-            <label htmlFor="title">Title</label>
-            <input id="title" name="title" type="text" />
-          </div>
-          <div>
-            <label htmlFor="description">Description</label>
-            <input id="description" name="description" type="text" />
-          </div>
-          <button type="submit"> Add Todo</button>
-        </form>
-      </div>
-    </>
+    <div>
+      <form action={addTodo}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input id="title" name="title" type="text" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <input id="description" name="description" type="text" />
+        </div>
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
   );
 }
 
